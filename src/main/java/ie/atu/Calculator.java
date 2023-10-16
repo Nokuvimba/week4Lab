2beedea5046d428e8813cdb723aae59df13d0f7e
Natalie <G00423951@atu.ie>
2023-10-16 11:20:54 +0100

    created a subtraction class

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -5,19 +5,20 @@ import java.util.Scanner;
 public class Calculator {
     public static void main(String[]args){
         System.out.println("Hello");
-        add();
+        subtract();
 
     }
-    public static void add()
+    public static void subtract()
     {
-        System.out.println("Please your first number :");
+        System.out.println("Subtraction:\n ");
+        System.out.println("Please enter your first number :");
         Scanner inputs= new Scanner(System.in);
         int firstNumber = inputs.nextInt();
 
-        System.out.println("Please your second :");
+        System.out.println("Please enter your second number:");
         int secondNumber =inputs.nextInt();
 
-        int total =firstNumber + secondNumber;
+        int total =firstNumber - secondNumber;
         System.out.println("The total is "+total);
     }
 
