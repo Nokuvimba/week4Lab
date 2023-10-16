fd17485ea776faedcc9ec6e0dae8e98ed935d023
Natalie <G00423951@atu.ie>
2023-10-16 10:56:59 +0100

    created multiple class

--- src/main/java/ie/atu/Calculator.java
+++ src/main/java/ie/atu/Calculator.java
@@ -6,10 +6,10 @@ public class Calculator {
     public static void main(String[]args){
         System.out.println("Hello");
         add();
-
+        multiple();
     }
     public static void add()
-    {
+    { System.out.println("addition: ");
         System.out.println("Please your first number :");
         Scanner inputs= new Scanner(System.in);
         int firstNumber = inputs.nextInt();
@@ -21,4 +21,17 @@ public class Calculator {
         System.out.println("The total is "+total);
     }
 
+    public static void multiple()
+    {
+        System.out.println("multiplication : ");
+        System.out.println("Please enter your first number :");
+        Scanner inputs= new Scanner(System.in);
+        int num1 = inputs.nextInt();
+
+        System.out.println("Please enter your second :");
+        int num2 =inputs.nextInt();
+
+        int multi =num1 * num2;
+        System.out.println("The multiple is "+multi);
+    }
 }
